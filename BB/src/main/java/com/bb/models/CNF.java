27c71f083f1c17a0ff773470ae595c5e264e679e
Tazio <tazio.fernandes@gmail.com>
2015-07-22 16:35:53 -0300

    Alteração de pastas

--- BB/src/main/java/com/bb/models/CNF.java
+++ BB/src/main/java/com/bb/models/CNF.java
@@ -1,4 +1,4 @@
-package models;
+package com.bb.models;
 
 /**
  * @author Tazio
--- BB/src/main/java/com/bb/views/agendamento/ScheduleView.java
+++ BB/src/main/java/com/bb/views/agendamento/ScheduleView.java
@@ -1,4 +1,4 @@
-package models.agendamento;
+package com.bb.views.agendamento;
 
 import java.io.Serializable;
 import java.util.Calendar;
