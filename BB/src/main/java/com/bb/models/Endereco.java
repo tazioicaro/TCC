8f4b0f00bed7bbc6694507c0b016e6ff163dd3d8
Tazio <tazio.fernandes@gmail.com>
2015-07-29 19:36:42 -0300

    Bean Endereço concluído

--- BB/src/main/java/com/bb/models/Endereco.java
+++ BB/src/main/java/com/bb/models/Endereco.java
@@ -2,6 +2,75 @@ package com.bb.models;
 
 import java.io.Serializable;
 
+import javax.inject.Named;
+import javax.persistence.Id;
+
+@Named
 public class Endereco implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Integer codigo;
+	private String rua;
+	private Integer numero; 
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	private String referencia;
+	
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getReferencia() {
+		return referencia;
+	}
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+	
+	
+	
 
 }
