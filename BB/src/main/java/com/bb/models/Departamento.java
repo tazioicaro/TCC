3b764486b1716ea6c03e2a39e3ad570a1b40b59d
Tazio <tazio.fernandes@gmail.com>
2015-07-29 13:27:35 -0300

    Bean Departamento concluído

--- BB/src/main/java/com/bb/models/Departamento.java
+++ BB/src/main/java/com/bb/models/Departamento.java
@@ -4,4 +4,29 @@ import java.io.Serializable;
 
 public class Departamento implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	private Integer codigo;
+	private String gerente;
+	private String nome;
+	
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getGerente() {
+		return gerente;
+	}
+	public void setGerente(String gerente) {
+		this.gerente = gerente;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
