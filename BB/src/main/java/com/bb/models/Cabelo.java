8f78de261db140fa15a0b8d7dacd37fd219f553a
Tazio <tazio.fernandes@gmail.com>
2015-07-29 13:41:29 -0300

    Bean Cliente concluído

--- BB/src/main/java/com/bb/models/Cabelo.java
+++
@@ -1,7 +0,0 @@
-package com.bb.models;
-
-import java.io.Serializable;
-
-public class Cabelo implements Serializable{
-
-}
--- BB/src/main/java/com/bb/models/Cliente.java
+++ BB/src/main/java/com/bb/models/Cliente.java
@@ -1,7 +1,116 @@
 package com.bb.models;
 
 import java.io.Serializable;
+import java.sql.Date;
 
-public class Cliente implements Serializable{
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+public class Cliente implements Serializable{	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="codigo_cli")
+	private Integer codigo;
+	private String cpf;
+	private String nome;
+	
+	@Column(name="data_nascimento")
+	private Date dataNascimento;
+	private String telefone;
+	private String celular;
+	private String email;
+	private Boolean sexo;
+	private String complemento;
+	
+	@JoinColumn(name="endereco_codigo_en")
+	@OneToOne
+	private Endereco endereco;
+	private String obs;
+	private Integer login;
+	private String senha;
+	
+	
+	
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Boolean getSexo() {
+		return sexo;
+	}
+	public void setSexo(Boolean sexo) {
+		this.sexo = sexo;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public String getObs() {
+		return obs;
+	}
+	public void setObs(String obs) {
+		this.obs = obs;
+	}
+	public Integer getLogin() {
+		return login;
+	}
+	public void setLogin(Integer login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 
 }
