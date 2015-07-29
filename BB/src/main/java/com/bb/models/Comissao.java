138aa89358872a19381b93744f0986ae4792e379
Tazio <tazio.fernandes@gmail.com>
2015-07-29 19:18:48 -0300

    bean Tipo comissão concluído

--- BB/src/main/java/com/bb/models/Comissao.java
+++ BB/src/main/java/com/bb/models/Comissao.java
@@ -2,6 +2,37 @@ package com.bb.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 public class Comissao implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="codigo_c")
+	private Integer codigo;
+	
+	@Column(name="tipo_comissao_codigo_tc")
+	@OneToMany
+	private TipoComissao tipoComissao;
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public TipoComissao getTipoComissao() {
+		return tipoComissao;
+	}
+
+	public void setTipoComissao(TipoComissao tipoComissao) {
+		this.tipoComissao = tipoComissao;
+	}
+	
+	
 }
