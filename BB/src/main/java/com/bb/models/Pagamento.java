520b2f8c3ef581661fa1821544730eaf8166f46d
Tazio <tazio.fernandes@gmail.com>
2015-08-04 21:10:48 -0300

    Ajustes JPA

--- BB/src/main/java/com/bb/models/Pagamento.java
+++ BB/src/main/java/com/bb/models/Pagamento.java
@@ -2,6 +2,68 @@ package com.bb.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Pagamento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long codigo;
+	
+	@ManyToOne
+	@JoinColumn(name="tpagamento_codigo", nullable=false)
+	private TipoPagamento tipoPagamento;
+	
+	@ManyToOne
+	@JoinColumn(name="pconta_codgo", nullable=false)
+	private PlanoConta planoConta;
+	
+	@ManyToOne
+	@JoinColumn(name="banco_codigo", nullable=false)
+	private Banco banco;
+	
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public TipoPagamento getTipoPagamento() {
+		return tipoPagamento;
+	}
+
+	public void setTipoPagamento(TipoPagamento tipoPagamento) {
+		this.tipoPagamento = tipoPagamento;
+	}
+
+	public PlanoConta getPlanoConta() {
+		return planoConta;
+	}
+
+	public void setPlanoConta(PlanoConta planoConta) {
+		this.planoConta = planoConta;
+	}
+
+	public Banco getBanco() {
+		return banco;
+	}
+
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+	
+	
+	
+	
 
 }
--- BB/src/main/java/com/bb/models/PlanoConta.java
+++ BB/src/main/java/com/bb/models/PlanoConta.java
@@ -2,8 +2,18 @@ package com.bb.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="plano_de_conta")
 public class PlanoConta implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private Long codigo;
+	
 
 }
