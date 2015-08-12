7fd06f8b8a39ffc663f83f4b47b717779b97daa7
Tazio <tazio.fernandes@gmail.com>
2015-08-11 21:28:17 -0300

    Cadastro Agendamento e Comissao

---
+++ BB/src/main/java/com/bb/controller/control/cadastros/CadastroComissaoBean.java
@@ -0,0 +1,9 @@
+package com.bb.controller.control.cadastros;
+
+import java.io.Serializable;
+
+public class CadastroComissaoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
