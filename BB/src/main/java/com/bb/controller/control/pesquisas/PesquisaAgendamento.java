1a601079daf090af8bf26e1e6ddf52dd41c109d6
Tazio <tazio.fernandes@gmail.com>
2015-08-11 22:25:52 -0300

    PesquisaAgendamento

---
+++ BB/src/main/java/com/bb/controller/control/pesquisas/PesquisaAgendamento.java
@@ -0,0 +1,10 @@
+package com.bb.controller.control.pesquisas;
+
+import java.io.Serializable;
+
+public class PesquisaAgendamento implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+}
