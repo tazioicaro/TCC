463643862f621d8571b59c5344e47059d74e3302
Tazio <tazio.fernandes@gmail.com>
2017-02-02 22:10:47 -0200

    CadastroClienteServices

--- BB/src/main/java/com/bb/controller/services/CadastroClienteService.java
+++ BB/src/main/java/com/bb/controller/services/CadastroClienteService.java
@@ -4,15 +4,22 @@ import java.io.Serializable;
 
 import javax.inject.Inject;
 
+import com.bb.controller.control.repository.Clientes;
 import com.bb.models.Cliente;
 
 public class CadastroClienteService implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Inject
 	private Clientes clientes;
 	
 	
 	public Cliente salvar (Cliente cliente){
+		return clientes.guardar(cliente);   
+		
 		
 	}
 
