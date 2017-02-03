411db2ee6cbe2ddcc90b45a424f9df74ce91111c
tazio.fernandes <tazio.fernandes@gmail.com>
2017-02-03 09:02:42 -0200

    Atualização do cadastroClienteService

--- BB/src/main/java/com/bb/controller/services/CadastroClienteService.java
+++ BB/src/main/java/com/bb/controller/services/CadastroClienteService.java
@@ -4,21 +4,32 @@ import java.io.Serializable;
 
 import javax.inject.Inject;
 
+
 import com.bb.controller.control.repository.Clientes;
+import com.bb.controller.util.jpa.Transactional;
 import com.bb.models.Cliente;
 
-public class CadastroClienteService implements Serializable {
-	
-	/**
-	 * 
-	 */
+public class CadastroClienteService implements Serializable {	
+
 	private static final long serialVersionUID = 1L;
+	
 	@Inject
 	private Clientes clientes;
 	
-	
+	@Transactional
 	public Cliente salvar (Cliente cliente){
-		return clientes.guardar(cliente);   
+		
+		Cliente usuarioExistenteNome = clientes.consultarNome(cliente.getNome());
+		Cliente usuatioExistenteEmail = clientes.porEmail(cliente.getEmail());
+		Cliente usuarioExistenteDoc = clientes.porDocIdentificacao(cliente.getDocumentoReceitaFederal());
+		
+		if(usuarioExistenteNome !=null && usuarioExistenteNome.equals(cliente.getNome())){
+			throw new NegocioException("Já existe um usuário com o nome informado. ");
+		}
+		
+		
+		cliente = this.clientes.guardar(cliente);
+		return cliente;   
 		
 		
 	}
