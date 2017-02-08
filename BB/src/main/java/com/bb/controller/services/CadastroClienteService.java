83b2f392cdd0bb5d43c11addb297886e0658c238
tazio.fernandes <tazio.fernandes@gmail.com>
2017-02-08 16:03:35 -0200

    Ajuste injeção nome classe de repositórios

--- BB/src/main/java/com/bb/controller/services/CadastroClienteService.java
+++ BB/src/main/java/com/bb/controller/services/CadastroClienteService.java
@@ -4,37 +4,37 @@ import java.io.Serializable;
 
 import javax.inject.Inject;
 
-
 import com.bb.controller.control.repository.Clientes;
 import com.bb.controller.util.jpa.Transactional;
 import com.bb.models.Cliente;
 
-public class CadastroClienteService implements Serializable {	
+public class CadastroClienteService implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
-	private Clientes clientes;
-	
+	private Clientes repositorioClientes;
+
 	@Transactional
-	public Cliente salvar (Cliente cliente){
-		
-		Cliente usuarioExistenteNome = clientes.consultarNome(cliente.getNome());
-		Cliente usuarioExistenteEmail = clientes.porEmail(cliente.getEmail());
-		Cliente usuarioExistenteDoc = clientes.porDocIdentificacao(cliente.getDocumentoReceitaFederal());
-		
-		if(usuarioExistenteNome !=null && usuarioExistenteNome.equals(cliente.getNome())){
+	public Cliente salvar(Cliente cliente) {
+
+		Cliente usuarioExistenteNome = repositorioClientes.consultarNome(cliente.getNome());
+		Cliente usuarioExistenteEmail = repositorioClientes.porEmail(cliente.getEmail());
+		Cliente usuarioExistenteDoc = repositorioClientes.porDocIdentificacao(cliente.getDocumentoReceitaFederal());
+
+		if (usuarioExistenteNome != null && usuarioExistenteNome.equals(cliente.getNome())) {
 			throw new NegocioException("Já existe um usuário com o nome informado. ");
-		} if (usuarioExistenteEmail != null && usuarioExistenteEmail.equals(cliente.getEmail())){
-			throw new  NegocioException ("Já existe um usuário com o e-mail informado. ");
-		} if (usuarioExistenteDoc !=null && usuarioExistenteDoc.equals(cliente.getDocumentoReceitaFederal())){
+		}
+		if (usuarioExistenteEmail != null && usuarioExistenteEmail.equals(cliente.getEmail())) {
+			throw new NegocioException("Já existe um usuário com o e-mail informado. ");
+		}
+		if (usuarioExistenteDoc != null && usuarioExistenteDoc.equals(cliente.getDocumentoReceitaFederal())) {
 			throw new NegocioException("Já existe um usuário do o Documento de Identificação informado");
-		}		
-		
-		cliente = this.clientes.guardar(cliente);
-		return cliente;   
-		
-		
+		}
+
+		cliente = this.repositorioClientes.guardar(cliente);
+		return cliente;
+
 	}
 
 }
