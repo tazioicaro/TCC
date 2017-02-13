449f9e3be5303020804d65133639ff199c211264
tazio.fernandes <tazio.fernandes@gmail.com>
2017-02-13 14:34:38 -0200

    Criação do Funcionário Filter com nome e cpf

---
+++ BB/src/main/java/com/bb/controller/control/repository/filter/FuncionarioFilter.java
@@ -0,0 +1,29 @@
+package com.bb.controller.control.repository.filter;
+
+import java.io.Serializable;
+
+public class FuncionarioFilter implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+	private String cpf;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+	
+
+}
