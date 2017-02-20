f215f62b8a55178aac6482009d03680c8ff175d7
tazio.fernandes <tazio.fernandes@gmail.com>
2017-02-20 17:06:41 -0300

    Inicio Pesquisa Funcionario

--- BB/src/main/java/com/bb/controller/control/pesquisas/PesquisaFuncionarioBean.java
+++ BB/src/main/java/com/bb/controller/control/pesquisas/PesquisaFuncionarioBean.java
@@ -2,7 +2,24 @@ package com.bb.controller.control.pesquisas;
 
 import java.io.Serializable;
 
+import javax.inject.Inject;
+
+import com.bb.controller.control.repository.Departamentos;
+import com.bb.controller.control.repository.Funcionarios;
+import com.bb.controller.control.repository.Grupos;
+
 public class PesquisaFuncionarioBean implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Funcionarios repositorioFuncionario;
+	
+	@Inject
+	private Grupos repositorioGrupos;
+	
+	@Inject
+	private Departamentos repositorioDepartamento;
+	
+	
 
 }
