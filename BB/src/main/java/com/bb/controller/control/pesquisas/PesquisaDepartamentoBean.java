b864f4f3ac69d340e3d33f6a082f19e7614236e5
tazio.fernandes <tazio.fernandes@gmail.com>
2017-03-07 16:21:36 -0300

    Departamento Salvando e agora falta editar o mesmo

--- BB/src/main/java/com/bb/controller/control/pesquisas/PesquisaDepartamentoBean.java
+++ BB/src/main/java/com/bb/controller/control/pesquisas/PesquisaDepartamentoBean.java
@@ -1,8 +1,83 @@
 package com.bb.controller.control.pesquisas;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+import com.bb.controller.control.repository.Departamentos;
+import com.bb.controller.control.repository.filter.DepartamentoFilter;
+import com.bb.models.Departamento;
+
+@Named
+@ViewScoped
 public class PesquisaDepartamentoBean implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Departamentos repositorioDepartamentos;
+	
+	
+	private LazyDataModel<Departamento> model;
+	private Departamento departamentoSelecionado;
+	
+	private DepartamentoFilter filtro;
+	
+	
+	
+	public PesquisaDepartamentoBean(){
+		
+		filtro = new DepartamentoFilter();
+		
+		model = new LazyDataModel<Departamento>(){			
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public List<Departamento> load(int first, int pageSize, 
+					String sortField, SortOrder sortOrder,
+					Map<String, Object> filters) {
+
+                         filtro.setPrimeiroRegistro(first);
+                         filtro.setQuantidadeRegistros(pageSize);
+                         filtro.setPropriedadeOrdenacao(sortField);
+                         filtro.setAscendente(SortOrder.ASCENDING.equals(sortOrder));
+                         
+                         setRowCount(repositorioDepartamentos.quantidadeFiltrados(filtro));
+				
+				
+				return repositorioDepartamentos.filtrados(filtro);
+			}
+			
+		};
+	}
+	
+	
+	
+	
+	//G&S
+	public Departamento getDepartamentoSelecionado() {
+		return departamentoSelecionado;
+	}
+	public void setDepartamentoSelecionado(Departamento departamentoSelecionado) {
+		this.departamentoSelecionado = departamentoSelecionado;
+	}
+	public LazyDataModel<Departamento> getModel() {
+		return model;
+	}
+	public void setModel(LazyDataModel<Departamento> model) {
+		this.model = model;
+	}
+	public DepartamentoFilter getFiltro() {
+		return filtro;
+	}
+	public void setFiltro(DepartamentoFilter filtro) {
+		this.filtro = filtro;
+	}
 
 }
--- BB/src/main/java/com/bb/controller/control/repository/Departamentos.java
+++ BB/src/main/java/com/bb/controller/control/repository/Departamentos.java
@@ -6,59 +6,102 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+import com.bb.controller.control.repository.filter.DepartamentoFilter;
 import com.bb.controller.util.jpa.Transactional;
 import com.bb.models.Departamento;
 
-
-
 public class Departamentos implements Serializable {
 
- 
-private static final long serialVersionUID = 1L;
- 
-@Inject
-private EntityManager manager;
- 
+	private static final long serialVersionUID = 1L;
 
- 
-public List<Departamento> porDepartamento(){
- 
- 
-return this.manager.createQuery("from Departamento where departamentoPai is null", Departamento.class).getResultList();
-}
+	@Inject
+	private EntityManager manager;
 
-public List<Departamento> todosGerentes(){
-	return this.manager.createQuery("from Departamento where departamentoPai is not null", Departamento.class).getResultList();
-}
- 
- 
-public Departamento porNome(String nome){
- 
-return this.manager.createQuery("from Departamento where lower(nome) =:nome", Departamento.class)
-.setParameter("nome", nome.toLowerCase()).getSingleResult();
-}
- 
- 
- 
-public List<Departamento> porGerente(Departamento departamentoPai) {
-return manager.createQuery("from Departamento where departamentoPai = :raiz", 
-Departamento.class).setParameter("raiz", departamentoPai).getResultList();
-}
- 
- 
- 
-@Transactional
-public  Departamento guardar (Departamento departamento){
- 
-return this.manager.merge(departamento);
-}
+	public List<Departamento> porDepartamento() {
 
+		return this.manager.createQuery("from Departamento where departamentoPai is null", Departamento.class)
+				.getResultList();
+	}
 
+	public List<Departamento> todosGerentes() {
+		return this.manager.createQuery("from Departamento where departamentoPai is not null", Departamento.class)
+				.getResultList();
+	}
 
+	public Departamento porNome(String nome) {
 
-public Departamento porId(Long valor) {
- 
-return this.manager.find(Departamento.class, valor);
-}
- 
+		return this.manager.createQuery("from Departamento where lower(nome) =:nome", Departamento.class)
+				.setParameter("nome", nome.toLowerCase()).getSingleResult();
+	}
+
+	public List<Departamento> porGerente(Departamento departamentoPai) {
+		return manager.createQuery("from Departamento where departamentoPai = :raiz", Departamento.class)
+				.setParameter("raiz", departamentoPai).getResultList();
+	}
+
+	@Transactional
+	public Departamento guardar(Departamento departamento) {
+
+		return this.manager.merge(departamento);
+	}
+
+	
+	public void removerDepartamento (Departamento departamentoSelecionado){
+		
+		manager.remove(manager.contains(departamentoSelecionado)? departamentoSelecionado : manager.merge(departamentoSelecionado));
+		
+	}
+	public Departamento porId(Long valor) {
+
+		return this.manager.find(Departamento.class, valor);
+	}
+
+	public Criteria criarCriteriaParaFiltro(DepartamentoFilter filtro) {
+
+		Session session = manager.unwrap(Session.class);
+
+		Criteria criteria = session.createCriteria(Departamento.class);
+
+		if (StringUtils.isNoneBlank(filtro.getNome())) {
+			criteria.add(Restrictions.ilike("nome", filtro.getNome()));
+		}
+
+		return criteria;
+
+	}
+
+	public int quantidadeFiltrados(DepartamentoFilter filtro) {
+		
+		Criteria criteria = criarCriteriaParaFiltro(filtro);
+		
+		criteria.setProjection(Projections.rowCount());
+
+		return ((Number) criteria.uniqueResult()).intValue();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Departamento> filtrados(DepartamentoFilter filtro) {
+
+		Criteria criteria = criarCriteriaParaFiltro(filtro);
+		criteria.setFirstResult(filtro.getPrimeiroRegistro());
+		criteria.setMaxResults(filtro.getQuantidadeRegistros());
+		
+		if(filtro.isAscendente() && filtro.getPropriedadeOrdenacao() !=null){
+			criteria.addOrder(Order.asc(filtro.getPropriedadeOrdenacao()));
+			
+		}	else if(filtro.getPropriedadeOrdenacao()!=null){
+			criteria.addOrder(Order.desc(filtro.getPropriedadeOrdenacao()));
+			
+		}
+
+		return criteria.list();
+	}
+
 }
